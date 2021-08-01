d033bc3843b61de61c96f162bf6769a1279ec058
HyojaeKwon-GIST20 <khj010909@gm.gist.ac.kr>
2021-08-02 00:10:56 +0900

    common dir

--- js/indexScript.js
+++ js/indexScript.js
@@ -2,5 +2,5 @@ let go = document.querySelector('#start')
 go.addEventListener("click",go_next)
 
 function go_next(){
-    location.href="./tutorials.html"
+    location.href="./common.tutorials.html"
 }
