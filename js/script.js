c7100eb2a699a36baaa63749f0865b7492ab9c65
HyojaeKwon-GIST20 <khj010909@gm.gist.ac.kr>
2021-08-02 00:59:03 +0900

    revise

--- js/script.js
+++ js/script.js
@@ -12,8 +12,19 @@ let go_1 = document.querySelector('#goto_1')
 
 let loc_2 = document.querySelector('#sec_2').offsetTop;
 let go_2 = document.querySelector('#goto_2')
+
+let loc_4 = document.querySelector('#sec_4').offsetTop;
+let go_4 = document.querySelector('#goto_4')
+
+let loc_5 = document.querySelector('#sec_5').offsetTop;
+let go_5 = document.querySelector('#goto_5')
+
+let loc_6 = document.querySelector('#sec_6').offsetTop;
+let go_6 = document.querySelector('#goto_6')
 //함수 실행
 go_3.addEventListener("click",function(){WindowScrollTo(loc_3)})
 go_2.addEventListener("click",function(){WindowScrollTo(loc_2)})
 go_1.addEventListener("click",function(){WindowScrollTo(loc_1)})
-
+go_4.addEventListener("click",function(){WindowScrollTo(loc_4)})
+go_5.addEventListener("click",function(){WindowScrollTo(loc_5)})
+go_6.addEventListener("click",function(){WindowScrollTo(loc_6)})
